05848a08814eaaead399a31d6e66a61da156f742
Danny Ben Shitrit <db@dannyben.com>
2026-01-06 13:00:01 +0000

    rubocop lint

--- lib/bashly/watch.rb
+++ lib/bashly/watch.rb
@@ -1,22 +1,22 @@
-require "listen"
+require 'listen'
 
 module Bashly
   # File system watcher - an ergonomic wrapper around the Listen gem
   class Watch
-    attr_reader :globs, :options
+    attr_reader :dirs, :options
 
     DEFAULT_OPTIONS = {
       force_polling: true,
-      latency: 1.0,
+      latency:       1.0,
     }.freeze
 
-    def initialize(*globs, **options)
+    def initialize(*dirs, **options)
       @options = DEFAULT_OPTIONS.merge(options).freeze
-      @globs   = globs.empty? ? ['.'] : globs
+      @dirs = dirs.empty? ? ['.'] : dirs
     end
 
-    def on_change(&block)
-      start(&block)
+    def on_change(&)
+      start(&)
       wait
     ensure
       stop
@@ -24,14 +24,14 @@ module Bashly
 
   private
 
-    def build_listener(&block)
-      listen.to(*globs, **options) do |modified, added, removed|
-        block.call changes(modified, added, removed)
+    def build_listener
+      listen.to(*dirs, **options) do |modified, added, removed|
+        yield changes(modified, added, removed)
       end
     end
 
     def start(&block)
-      raise ArgumentError, "block required" unless block
+      raise ArgumentError, 'block required' unless block
 
       @listener = build_listener(&block)
       @listener.start
